aa14baeded46920b92d420146316d7332fc46a45
Carolyn Sun <capri3sun@Carolyns-MacBook-Air.local>
2023-08-22 20:13:28 -0700

    Add part 1 anecdotes exercises

--- part1/courseinfo/my-app/src/App.js
+++ part1/courseinfo/my-app/src/App.js
@@ -1,61 +1,27 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part partName={props.parts[0].name} numOfExercises={props.parts[0].exercises} />
-      <Part partName={props.parts[1].name} numOfExercises={props.parts[1].exercises} />
-      <Part partName={props.parts[2].name} numOfExercises={props.parts[2].exercises} />
-    </div>
-  )
-}
+const Display = props => <div>{props.value}</div>
 
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.partName} {props.numOfExercises}
-      </p>
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
